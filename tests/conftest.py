2244ec125abac40342c2e3c74e680863be432391
Richard Gow <richardstewartgow@gmail.com>
2024-01-15 14:19:42 +0000

    remove pycache

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,24 @@
+from typing import Generator
+from typing import Any
+
 import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
+
 def start_application():
     app = FastAPI()
     return app
+
+@pytest.fixture(scope="function")
+def app() -> Generator[FastAPI, Any, None]:
+
+    _app = start_application()
+    yield _app
+
+@pytest.fixture(scope="function")
+def client(
+        app: FastAPI
+) -> Generator[TestClient, Any, None]:
+    with TestClient(app) as client:
+        yield client
--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,4 +1,3 @@
-
-
 def test_hello_world(client):
     response = client.get("/")
+    assert response.json()['msg'] == "Hello world"
