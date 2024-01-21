4f4815d7677319ddd9271d875ff5e5a3f4d88577
Richard Gow <richardstewartgow@gmail.com>
2024-01-21 16:04:10 +0000

    fixed but unsure, turns out you just cannot use either di in your own functions unless you pass down deps... hmmm

--- app/strategies/hello_world_strategy.py
+++ app/strategies/hello_world_strategy.py
@@ -1,11 +1,4 @@
-#from app.actions.json_message_action import run as json_make
-from fastapi import Depends
-from app.containers import Container
-from app.actions import json_message_action
-from dependency_injector.wiring import inject, Provide
+from app.actions.json_message_action import JsonMessageAction
 
-@inject
-def run(
-    json_message_action: json_message_action = Depends(Provide[Container.json_message_action])
-):
-    return json_message_action.run("Hello world")
+def run():
+    return JsonMessageAction().run("Hello world")
--- app/strategies/reverse_strategy.py
+++ app/strategies/reverse_strategy.py
@@ -1,9 +1,4 @@
-#from app.actions.json_message_action import run as json_make
-from fastapi import Depends
-from app.containers import Container
-from app.actions import json_message_action
-from dependency_injector.wiring import inject, Provide
-
+from app.actions.json_message_action import JsonMessageAction
 
 """
 Reverse the input payload into a new string using slicing as strings are immutate iterateble objects then
@@ -11,9 +6,5 @@ use action to return as a Json response
 
 payload (str): string to reverse
 """
-@inject
-def run(
-    payload: str,
-    json_message_action: json_message_action = Depends(Provide[Container.json_message_action])
-):
-    return json_message_action.run(payload[::-1])
+def run(payload: str):
+    return JsonMessageAction().run(payload[::-1])
